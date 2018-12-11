3c24a2194e7a84da02a595f41d4a52d039ec243c
SDH <sdh734@qq.com>
2018-12-11 22:27:53 +0800

    First Commit

---
+++ src/main/java/edu/smxy/associationmanagement/AssociationmanagementApplication.java
@@ -0,0 +1,12 @@
+package edu.smxy.associationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociationmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssociationmanagementApplication.class, args);
+    }
+}
