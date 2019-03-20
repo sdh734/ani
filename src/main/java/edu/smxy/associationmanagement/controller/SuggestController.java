99e2d58944bd9237be59b41d42ea561e3a808fbe
SDH <sdh734@qq.com>
2019-03-20 23:20:24 +0800

    修改finduser方法为不需要RequestBody

--- src/main/java/edu/smxy/associationmanagement/controller/SuggestController.java
+++ src/main/java/edu/smxy/associationmanagement/controller/SuggestController.java
@@ -1,131 +1,171 @@
 package edu.smxy.associationmanagement.controller;
 
-import org.springframework.boot.autoconfigure.*;
-import edu.smxy.associationmanagement.services.suggest.*;
-import org.springframework.beans.factory.annotation.*;
-import edu.smxy.associationmanagement.services.association.*;
-import edu.smxy.associationmanagement.services.users.*;
-import java.util.*;
-import org.springframework.web.bind.annotation.*;
-import edu.smxy.associationmanagement.domain.*;
-import javax.servlet.http.*;
-import java.text.*;
+import edu.smxy.associationmanagement.domain.JSONResult;
+import edu.smxy.associationmanagement.domain.Suggest;
+import edu.smxy.associationmanagement.domain.SuggestResult;
+import edu.smxy.associationmanagement.domain.User;
+import edu.smxy.associationmanagement.services.association.AssociationService;
+import edu.smxy.associationmanagement.services.suggest.SuggestService;
+import edu.smxy.associationmanagement.services.users.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author SDH
+ */
 @ResponseBody
 @RestController
 @EnableAutoConfiguration
-public class SuggestController
-{
+public class SuggestController {
     @Autowired
     SuggestService suggestService;
     @Autowired
     AssociationService associationService;
     @Autowired
     UserService userService;
-    
-    @RequestMapping({ "/getSelectListByRadioValue" })
+
+    @RequestMapping({"/getSelectListByRadioValue"})
     public JSONResult getSelectListByRadioValue(final int type) {
         if (type == 0) {
-            return JSONResult.build(200, "0", (Object)this.associationService.getAll());
+            return JSONResult.build(200, "0", this.associationService.getAll());
         }
-        final List<User> userList = (List<User>)this.userService.getAll();
+        final List<User> userList = this.userService.getAll();
         final List<User> userList2 = new ArrayList<User>();
         for (final User user : userList) {
             if (user.getType() == 1) {
                 userList2.add(user);
             }
         }
-        return JSONResult.build(200, "1", (Object)userList2);
+        return JSONResult.build(200, "1", userList2);
     }
-    
-    @RequestMapping({ "/addSuggestAssToSL" })
+
+    @RequestMapping({"/addSuggestAssToSL"})
     public JSONResult addSuggestAssToSL(final Suggest suggest) {
         final int result = this.suggestService.insert(suggest);
         if (result > 0) {
-            return JSONResult.build(200, "ok", (Object)null);
+            return JSONResult.build(200, "ok", null);
         }
-        return JSONResult.build(500, "error", (Object)null);
+        return JSONResult.build(500, "error", null);
     }
-    
-    @RequestMapping({ "/addSuggestSLToAss" })
+
+    @RequestMapping({"/addSuggestSLToAss"})
     public JSONResult addSuggestSLToAss(final Suggest suggest) {
         final int result = this.suggestService.insert(suggest);
         if (result > 0) {
-            return JSONResult.build(200, "ok", (Object)null);
+            return JSONResult.build(200, "ok", null);
         }
-        return JSONResult.build(500, "error", (Object)null);
+        return JSONResult.build(500, "error", null);
     }
-    
-    @RequestMapping({ "/addSuggestAssToAss" })
+
+    @RequestMapping({"/addSuggestAssToAss"})
     public JSONResult addSuggestAssToAss(final Suggest suggest) {
         final int result = this.suggestService.insert(suggest);
         if (result > 0) {
-            return JSONResult.build(200, "ok", (Object)null);
+            return JSONResult.build(200, "ok", null);
         }
-        return JSONResult.build(500, "error", (Object)null);
+        return JSONResult.build(500, "error", null);
     }
-    
-    @RequestMapping({ "/getAllSuggestByAuthorId" })
+
+    @RequestMapping({"/getAllSuggestByAuthorId"})
     public JSONResult getAllSuggestByAuthorId(final Integer authorid) {
-        final List<Suggest> suggests = (List<Suggest>)this.suggestService.getAllSuggestByAuthorId(authorid);
+        final List<Suggest> suggests = this.suggestService.getAllSuggestByAuthorId(authorid);
         final List<SuggestResult> suggestResults = new ArrayList<SuggestResult>();
         for (final Suggest suggest : suggests) {
             final SuggestResult suggestResult = new SuggestResult(suggest);
             switch (suggest.getType()) {
                 case 0: {
-                    suggestResult.setAuthorname(this.associationService.selectByPrimaryKey(suggest.getAuthorid()).getAssociationName());
-                    suggestResult.setReveivename(this.userService.selectUserById((int)suggest.getReceiveid()).getName());
+                    suggestResult.setAuthorname(
+                            this.associationService
+                                    .selectByPrimaryKey(suggest.getAuthorid())
+                                    .getAssociationName());
+                    suggestResult.setReveivename(
+                            this.userService.selectUserById(suggest.getReceiveid()).getName());
                     break;
                 }
                 case 1: {
-                    suggestResult.setAuthorname(this.userService.selectUserById((int)suggest.getAuthorid()).getName());
-                    suggestResult.setReveivename(this.associationService.selectByPrimaryKey(suggest.getReceiveid()).getAssociationName());
+                    suggestResult.setAuthorname(
+                            this.userService.selectUserById(suggest.getAuthorid()).getName());
+                    suggestResult.setReveivename(
+                            this.associationService
+                                    .selectByPrimaryKey(suggest.getReceiveid())
+                                    .getAssociationName());
                     break;
                 }
                 case 2: {
-                    suggestResult.setAuthorname(this.associationService.selectByPrimaryKey(suggest.getAuthorid()).getAssociationName());
-                    suggestResult.setReveivename(this.associationService.selectByPrimaryKey(suggest.getReceiveid()).getAssociationName());
+                    suggestResult.setAuthorname(
+                            this.associationService
+                                    .selectByPrimaryKey(suggest.getAuthorid())
+                                    .getAssociationName());
+                    suggestResult.setReveivename(
+                            this.associationService
+                                    .selectByPrimaryKey(suggest.getReceiveid())
+                                    .getAssociationName());
                     break;
                 }
             }
             suggestResults.add(suggestResult);
         }
-        return JSONResult.build(200, "ok", (Object)suggestResults);
+        return JSONResult.build(200, "ok", suggestResults);
     }
-    
-    @RequestMapping({ "/getAllSuggestByReceiveId" })
+
+    @RequestMapping({"/getAllSuggestByReceiveId"})
     public JSONResult getAllSuggestByReceiveId(final Integer receiveid) {
-        final List<Suggest> suggests = (List<Suggest>)this.suggestService.getAllSuggestByReceiveId(receiveid);
+        final List<Suggest> suggests = this.suggestService.getAllSuggestByReceiveId(receiveid);
         final List<SuggestResult> suggestResults = new ArrayList<SuggestResult>();
         for (final Suggest suggest : suggests) {
             final SuggestResult suggestResult = new SuggestResult(suggest);
             switch (suggest.getType()) {
                 case 0: {
-                    suggestResult.setAuthorname(this.associationService.selectByPrimaryKey(suggest.getAuthorid()).getAssociationName());
-                    suggestResult.setReveivename(this.userService.selectUserById((int)suggest.getReceiveid()).getName());
+                    suggestResult.setAuthorname(
+                            this.associationService
+                                    .selectByPrimaryKey(suggest.getAuthorid())
+                                    .getAssociationName());
+                    suggestResult.setReveivename(
+                            this.userService.selectUserById(suggest.getReceiveid()).getName());
                     break;
                 }
                 case 1: {
-                    suggestResult.setAuthorname(this.userService.selectUserById((int)suggest.getAuthorid()).getName());
-                    suggestResult.setReveivename(this.associationService.selectByPrimaryKey(suggest.getReceiveid()).getAssociationName());
+                    suggestResult.setAuthorname(
+                            this.userService.selectUserById(suggest.getAuthorid()).getName());
+                    suggestResult.setReveivename(
+                            this.associationService
+                                    .selectByPrimaryKey(suggest.getReceiveid())
+                                    .getAssociationName());
                     break;
                 }
                 case 2: {
-                    suggestResult.setAuthorname(this.associationService.selectByPrimaryKey(suggest.getAuthorid()).getAssociationName());
-                    suggestResult.setReveivename(this.associationService.selectByPrimaryKey(suggest.getReceiveid()).getAssociationName());
+                    suggestResult.setAuthorname(
+                            this.associationService
+                                    .selectByPrimaryKey(suggest.getAuthorid())
+                                    .getAssociationName());
+                    suggestResult.setReveivename(
+                            this.associationService
+                                    .selectByPrimaryKey(suggest.getReceiveid())
+                                    .getAssociationName());
                     break;
                 }
             }
             suggestResults.add(suggestResult);
         }
-        return JSONResult.build(200, "ok", (Object)suggestResults);
+        return JSONResult.build(200, "ok", suggestResults);
     }
-    
-    @RequestMapping({ "/updateSuggest" })
+
+    @RequestMapping({"/updateSuggest"})
     public JSONResult updateSuggest(final HttpServletRequest request) {
         final Suggest suggest = new Suggest();
         final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        suggest.setCreatetime(simpleDateFormat.parse(request.getParameter("createtime").replace("T", " "), new ParsePosition(0)));
+        suggest.setCreatetime(
+                simpleDateFormat.parse(
+                        request.getParameter("createtime").replace("T", " "), new ParsePosition(0)));
         suggest.setId(Integer.valueOf(request.getParameter("id")));
         suggest.setAuthorid(Integer.valueOf(request.getParameter("authorid")));
         suggest.setReceiveid(Integer.valueOf(request.getParameter("receiveid")));
@@ -133,14 +173,14 @@ public class SuggestController
         suggest.setType(Integer.valueOf(request.getParameter("type")));
         final int result = this.suggestService.updateByPrimaryKeyWithBLOBs(suggest);
         if (result > 0) {
-            return JSONResult.build(200, "ok", (Object)null);
+            return JSONResult.build(200, "ok", null);
         }
-        return JSONResult.build(500, "error", (Object)null);
+        return JSONResult.build(500, "error", null);
     }
-    
-    @RequestMapping({ "/deleteSuggestById" })
+
+    @RequestMapping({"/deleteSuggestById"})
     public JSONResult deleteSuggestById(final int id) {
         this.suggestService.deleteByPrimaryKey(id);
-        return JSONResult.build(200, "ok", (Object)null);
+        return JSONResult.build(200, "ok", null);
     }
 }
--- src/main/java/edu/smxy/associationmanagement/controller/UserController.java
+++ src/main/java/edu/smxy/associationmanagement/controller/UserController.java
@@ -1,59 +1,66 @@
 package edu.smxy.associationmanagement.controller;
 
-import org.springframework.boot.autoconfigure.*;
-import edu.smxy.associationmanagement.services.users.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.web.bind.annotation.*;
-import edu.smxy.associationmanagement.domain.*;
-import javax.servlet.http.*;
-import edu.smxy.associationmanagement.utils.*;
+import edu.smxy.associationmanagement.domain.JSONResult;
+import edu.smxy.associationmanagement.domain.MyCookie;
+import edu.smxy.associationmanagement.domain.User;
+import edu.smxy.associationmanagement.services.users.UserService;
+import edu.smxy.associationmanagement.utils.CookieUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author SDH
+ */
 @ResponseBody
 @RestController
 @EnableAutoConfiguration
-public class UserController
-{
+public class UserController {
     @Autowired
     private UserService userService;
-    
-    @PostMapping({ "/adduser" })
+
+    @PostMapping({"/adduser"})
     public int addUser(final User user) {
         return this.userService.addUser(user);
     }
-    
-    @PostMapping({ "/finduser" })
-    public JSONResult findUser(@RequestBody final MyCookie cookie, final HttpServletRequest request) {
+
+    @PostMapping({"/finduser"})
+    public JSONResult findUser(final MyCookie cookie) {
         final User user = new User();
         final int userid = cookie.getUserId();
-        if (userid + "" == "") {
-            final JSONResult jsonResult = JSONResult.build(500, "\u672a\u767b\u5f55", (Object)null);
-            return jsonResult;
+        if ("".equals(userid + "")) {
+            return JSONResult.build(500, "未登录", null);
         }
         user.setId(userid);
-        final User result = this.userService.selectUserById((int)user.getId());
+        final User result = this.userService.selectUserById(user.getId());
         if (result != null) {
-            final JSONResult jsonResult2 = JSONResult.build(200, "ok", (Object)result);
-            return jsonResult2;
+            return JSONResult.build(200, "ok", result);
         }
-        final JSONResult jsonResult2 = JSONResult.build(500, "\u65e0\u6cd5\u627e\u5230id\u5bf9\u5e94\u7684\u7528\u6237", (Object)null);
-        return jsonResult2;
+        return JSONResult.build(500, "无法找到_ID_对应的用户", null);
     }
-    
-    @PostMapping({ "/login" })
-    public JSONResult login(final String account, final String password, final HttpServletRequest request, final HttpServletResponse response) {
+
+    @PostMapping({"/login"})
+    public JSONResult login(
+            final String account, final String password, final HttpServletRequest request) {
         final User user = new User();
         user.setAccount(account);
         user.setPassword(password);
         final User result = this.userService.findUser(user);
         if (result != null) {
-            request.getSession().setAttribute("userid", (Object)result.getId());
-            return JSONResult.build(200, "", (Object)new MyCookie((int)result.getId(), request.getSession().getId()));
+            request.getSession().setAttribute("userid", result.getId());
+            return JSONResult.build(200, "", new MyCookie(result.getId(), request.getSession().getId()));
         }
-        return JSONResult.build(500, "\u627e\u4e0d\u5230\u8be5\u7528\u6237", (Object)null);
+        return JSONResult.build(500, "找不到该用户", null);
     }
-    
-    @PostMapping({ "/logout" })
-    public int logout(final String userid, final HttpServletRequest request, final HttpServletResponse response) {
+
+    @PostMapping({"/logout"})
+    public int logout(
+            final String userid, final HttpServletRequest request, final HttpServletResponse response) {
         request.getSession().removeAttribute("userid");
         CookieUtils.writeCookie(response, "userid", "");
         CookieUtils.writeCookie(response, "sessionid", "");
